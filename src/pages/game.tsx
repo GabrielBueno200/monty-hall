c636d7dcac435cd7be96f5dba719706f0ca3ab87
GabrielBueno200 <gabrielbueno200@gmail.com>
2021-12-19 00:13:16 -0300

    added restart game button

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import type { NextPage } from 'next'
-import { Container, Doors } from '../styles/pages/game'
+import { Container, Doors, RestartButton } from '../styles/pages/game'
 import { useGame } from '../contexts/game'
 import Door from '../components/Door'
+import { useRouter } from 'next/router'
 
 const Game: NextPage = () => {
+  const router = useRouter()
+
   const { doors } = useGame()
 
   return (
     <Container>
       <Doors>
-        {doors.map((door, idx) => {
-          return <Door {...door} key={idx} />
-        })}
+        {doors.map((door, idx) => (
+          <Door {...door} key={idx} />
+        ))}
       </Doors>
+
+      <RestartButton onClick={() => router.push('/')}>
+        Reiniciar jogo
+      </RestartButton>
     </Container>
   )
 }
--- src/styles/pages/game.ts
+++ src/styles/pages/game.ts
@@ -3,9 +3,26 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `
 
 export const Doors = styled.div`
   display: flex;
+  align-self: stretch;
   justify-content: space-around;
+  flex-wrap: wrap;
+`
+
+export const RestartButton = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+  background-color: #c0332c;
+  color: #fff;
+  font-size: 2rem;
+  border: none;
+  padding: 10px 25px;
+  border-radius: 5px;
 `
