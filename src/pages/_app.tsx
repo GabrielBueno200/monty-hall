92b684c40eff86b508111f355a3cfb6800e2c30a
GabrielBueno200 <gabrielbueno200@gmail.com>
2021-12-19 01:00:39 -0300

    fixed global title and removed unused dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
-import { ToastContainer } from 'react-toastify'
 
 import { GameProvider } from '../contexts/game'
 
 import { ThemeProvider } from 'styled-components'
 import theme from '../styles/theme'
 import GlobalStyle from '../styles/global'
-import 'react-toastify/dist/ReactToastify.css'
+import Head from 'next/head'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <GameProvider>
       <ThemeProvider theme={theme}>
+        <Head>
+          <title>Monty Hall</title>
+        </Head>
         <Component {...pageProps} />
-        <ToastContainer />
         <GlobalStyle />
       </ThemeProvider>
     </GameProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,13 +43,12 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
 
-          <title>Monty Hall Game</title>
-
           <link
             href="http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900"
             rel="stylesheet"
           />
         </Head>
+
         <body>
           <Main />
           <NextScript />
